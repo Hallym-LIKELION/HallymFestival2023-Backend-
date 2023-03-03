d9ab63545e19bd8af32f0ebb074d296ca501e075
dongheonlee <sirius506775@gmail.com>
2023-03-04 04:56:57 +0900

    init : 초기 환경 구성

---
+++ src/main/java/com/hallym/festival/FestivalApplication.java
@@ -0,0 +1,13 @@
+package com.hallym.festival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestivalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestivalApplication.class, args);
+	}
+
+}

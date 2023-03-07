e537e2d60cc6983e4da01cd78af263a6f415f881
dongheonlee <sirius506775@gmail.com>
2023-03-08 02:03:04 +0900

    feat : JPA 매핑 테이블에 대한 insert 테스트 코드

--- src/test/java/com/hallym/festival/FestivalApplicationTests.java
+++ src/test/java/com/hallym/festival/FestivalApplicationTests.java
@@ -1,13 +1,43 @@
 package com.hallym.festival;
 
+import com.hallym.festival.domain.booth.Booth;
+import com.hallym.festival.domain.booth.BoothRepository;
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
+@Log4j2
 class FestivalApplicationTests {
 
+	@Autowired
+	private BoothRepository boothRepository;
+
 	@Test
-	void contextLoads() {
+	public void insertTests() throws Exception{
+
+		Booth booth = Booth.builder()
+				.name("물풍선던기지")
+				.description("대신 맞아드립니다.")
+				.image("http://example.com/test11.png")
+				.build();
+
+		log.info("------------------");
+		log.info(booth);
+
+		Booth savedBooth = boothRepository.save(booth);
+
+		List<Booth> boothList = boothRepository.findAll();
+
+		log.info("-------------------save-----------");
+		log.info(savedBooth);
+
+		log.info(boothList);
+
 	}
+
 
 }

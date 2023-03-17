06c70f68d331a11dbf6160f04c6c757fab121bae
dongheonlee <sirius506775@gmail.com>
2023-03-18 03:57:29 +0900

    refactor : Service 클래스 Been 등록을 위한 어노테이션 추가

--- src/main/java/com/hallym/festival/domain/comment/service/CommentService.java
+++ src/main/java/com/hallym/festival/domain/comment/service/CommentService.java
@@ -2,9 +2,15 @@ package com.hallym.festival.domain.comment.service;
 
 import com.hallym.festival.domain.comment.entity.Comment;
 import com.hallym.festival.domain.comment.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class CommentService {
 
     private CommentRepository commentRepository;

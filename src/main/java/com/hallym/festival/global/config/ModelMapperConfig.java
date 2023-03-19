f1f53b2c792026f585ebe6060c1c30c1eb5f90c7
dongheonlee <sirius506775@gmail.com>
2023-03-19 19:18:37 +0900

    feat : ModelMapper 라이브러리 사용을 위한 Configuration 파일 생성
    - setter를 이용하지 않고도 매핑을 할 수 있게끔 AccessLevel를 private 변경

---
+++ src/main/java/com/hallym/festival/global/config/ModelMapperConfig.java
@@ -0,0 +1,24 @@
+package com.hallym.festival.global.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean //메소드 실행 결과로 반환된 객체를 스프링의 빈으로 등록
+    public ModelMapper getMapper() { // modelMapper를 반환하는 메소드
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.LOOSE) // LOOSE 전략
+                .setSkipNullEnabled(true) // null인 필드는 skip
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE); //Access level PRIVATE 으로 변경
+
+        return modelMapper;
+    }
+
+    // setter 없이도 필드명이 같을 때 자동 매핑 처리
+}

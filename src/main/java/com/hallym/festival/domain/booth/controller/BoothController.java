99b9effaf2b2681615fe95ae283bf2aa5a0ca107
dongheonlee <sirius506775@gmail.com>
2023-03-20 01:04:22 +0900

    fix : comment & like method 추가

--- src/main/java/com/hallym/festival/domain/booth/controller/BoothController.java
+++ src/main/java/com/hallym/festival/domain/booth/controller/BoothController.java
@@ -3,17 +3,24 @@ package com.hallym.festival.domain.booth.controller;
 import com.hallym.festival.domain.booth.dto.BoothDTO;
 import com.hallym.festival.domain.booth.entity.Booth;
 import com.hallym.festival.domain.booth.service.BoothService;
+import com.hallym.festival.domain.comment.dto.CommentRequestDto;
+import com.hallym.festival.domain.comment.dto.CommentResponseDto;
+import com.hallym.festival.domain.comment.service.CommentService;
+import com.hallym.festival.domain.likes.dto.LikesResponseDto;
+import com.hallym.festival.domain.likes.service.LikeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.validation.Valid;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
+import java.util.Optional;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("api/booth")
@@ -22,6 +29,8 @@ import java.util.List;
 public class BoothController {
 
     private final BoothService boothService;
+    private final CommentService commentService;
+    private final LikeService likeService;
 
     @GetMapping("/list")
     public List<Booth> getAllBooths() {
@@ -44,6 +53,47 @@ public class BoothController {
         redirectAttributes.addFlashAttribute("result");
 
         return "redirect:/booths/list";
+    }
+
+
+    @PostMapping("/{id}/comments")
+    public CommentResponseDto createComment
+            (@PathVariable(name="id") Long boothId, @RequestBody CommentRequestDto commentRequestDto, HttpServletRequest request) throws Exception {
+        return commentService.create(boothId, commentRequestDto, request);
+    }
+
+    @PostMapping("/{id}/likes")
+    public LikesResponseDto likeCreate(@PathVariable Long id, HttpServletRequest request, HttpServletResponse response) {
+        Optional<Cookie> boothCookie = likeService.findBoothCookie(request, id);
+        if (boothCookie.isPresent()) {
+            throw new IllegalArgumentException("이미 좋아요 누름");
+        }
+        LikesResponseDto likes = likeService.create(id);
+        Cookie keyCookie = new Cookie(id.toString(), likes.getCookieKey());
+        keyCookie.setMaxAge(14*60*60*24); // 2주일
+        keyCookie.setPath("/");
+        response.addCookie(keyCookie);
+        return likes;
+    }
+
+    @DeleteMapping("/{id}/likes")
+    public HttpStatus likeDelete(@PathVariable Long id, HttpServletRequest request, HttpServletResponse response) {
+        Optional<Cookie> boothCookie = likeService.findBoothCookie(request, id);
+        if (boothCookie.isPresent()) {
+            Cookie userCookie = boothCookie.get();
+            String CookieKey = userCookie.getValue();
+            likeService.delete(id, CookieKey);
+
+            Cookie keyCookie = new Cookie(id.toString(), null);
+            keyCookie.setMaxAge(0);
+            keyCookie.setPath("/");
+            response.addCookie(keyCookie);
+        }
+        else {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return HttpStatus.BAD_REQUEST;
+        }
+        return HttpStatus.OK;
     }
 
 

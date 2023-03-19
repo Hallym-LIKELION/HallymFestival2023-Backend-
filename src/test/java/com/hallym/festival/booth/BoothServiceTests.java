4042cbc0378e538a771193d16c885bd37998984c
dongheonlee <sirius506775@gmail.com>
2023-03-20 05:15:16 +0900

    test :  Booth 조회, 수정, 삭제 기능 테스트 코드 작성

--- src/test/java/com/hallym/festival/booth/BoothServiceTests.java
+++ src/test/java/com/hallym/festival/booth/BoothServiceTests.java
@@ -1,6 +1,7 @@
 package com.hallym.festival.booth;
 
 import com.hallym.festival.domain.booth.dto.BoothDTO;
+import com.hallym.festival.domain.booth.entity.BoothType;
 import com.hallym.festival.domain.booth.service.BoothService;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
@@ -15,16 +16,59 @@ public class BoothServiceTests {
     BoothService boothService;
 
     @Test
-    public void testResister(){
+    public void testRegister(){
+
+//        BoothServiceImpl을 감싸서 만든 클래스인지 확인
+//        log.info(boothService.getClass().getName()); // BoothServiceImpl$$EnhancerBySpringCGLIB$$75889ac5
 
         BoothDTO boothDTO = BoothDTO.builder()
-                .booth_title("3점왕 왕좌에 올라라")
-                .booth_content("지정된 3점 슛 라인에서 공 10개를 던져서 가장 많이 넣은 제왕을 뽑습니다.")
-                .writer("레이업애호가")
-                .booth_type("게임")
+                .booth_title("타코야끼")
+                .booth_content("새벽 4시 45분")
+                .writer("주펄")
+                .booth_type(BoothType.푸드트럭)
                 .build();
 
-        boothService.register(boothDTO);
+        Long bno = boothService.register(boothDTO);
+
+        log.info("bno: " + bno);
     }
 
+    @Test
+    public void testModify() {
+
+        //변경에 필요한 데이터만
+        BoothDTO boothDTO = BoothDTO.builder()
+                .bno(3L)
+                .booth_title("현재 4시 46분")
+                .booth_content("이것만 되면 CRUD 끝이야")
+                .writer("딱 끝내자")
+                .booth_type(BoothType.푸드트럭)
+                .active(false)
+                .build();
+
+        boothService.modify(boothDTO);
+
+        log.info(boothDTO);
+    }
+
+    @Test
+    public void testGetOne(){
+
+        Long bno = 2L;
+
+        BoothDTO boothDTO = boothService.getOne(bno);
+
+        log.info(boothDTO);
+
+    }
+
+    @Test
+    public void testRemove(){
+
+        Long bno = 2L;
+
+        boothService.remove(bno);
+
+        log.info(boothService.getOne(bno));
+    }
 }

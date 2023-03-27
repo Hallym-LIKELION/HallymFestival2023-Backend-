0d375e9730fe2a8c493a4732836c05bde2b1102a
dongheonlee <sirius506775@gmail.com>
2023-03-27 19:38:51 +0900

    feat : Querydsl를 사용한 검색 기능 구현 - BoothSearch 인터페이스와 BoothSearchImpl 구현체 구조 - 검색 테스트 코드 작성

--- src/test/java/com/hallym/festival/repository/BoothRepositoryTests.java
+++ src/test/java/com/hallym/festival/repository/BoothRepositoryTests.java
@@ -176,9 +176,61 @@ public class BoothRepositoryTests {
 
 		Long bno = 1L;
 
-		commentRepository.deleteByBooth_Bno(bno);
+//		commentRepository.deleteByBooth_Bno(bno);
+//
+//		boothRepository.deleteById(bno);
 
-		boothRepository.deleteById(bno);
+		//삭제가 아니라 is_deleted 만 변경
 
+	}
+
+
+	@Test
+	public void testSearch1() {
+
+		//2 page order by bno desc
+		Pageable pageable = PageRequest.of(1,10, Sort.by("bno").descending());
+
+		boothRepository.search1(pageable);
+
+	}
+
+	@Test
+	public void testSearchAll() {
+
+		String[] types = {"t","c","w"};
+
+		String keyword = "1";
+
+		Pageable pageable = PageRequest.of(0,10, Sort.by("bno").descending());
+
+		Page<Booth> result = boothRepository.searchAll(types, keyword, pageable );
+
+	}
+
+	@Test
+	public void testSearchAll2() {
+
+		String[] types = {"t","c","w"};
+
+		String keyword = "1";
+
+		Pageable pageable = PageRequest.of(0,10, Sort.by("bno").descending());
+
+		Page<Booth> result = boothRepository.searchAll(types, keyword, pageable );
+
+		//total pages
+		log.info(result.getTotalPages());
+
+		//pag size
+		log.info(result.getSize());
+
+		//pageNumber
+		log.info(result.getNumber());
+
+		//prev next
+		log.info(result.hasPrevious() +": " + result.hasNext());
+
+		result.getContent().forEach(board -> log.info(board));
 	}
 }

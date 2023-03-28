74f1ed40b12bfa41a61bc664d2d1909fae206d89
mythpoy <okmlnsunok@gmail.com>
2023-03-28 22:48:27 +0900

    feat : VisitComment 관련 Dto 생성

---
+++ src/main/java/com/hallym/festival/domain/visitcomment/dto/VisitCommentPasswordDto.java
@@ -0,0 +1,15 @@
+package com.hallym.festival.domain.visitcomment.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class VisitCommentPasswordDto {
+    private String password;
+}

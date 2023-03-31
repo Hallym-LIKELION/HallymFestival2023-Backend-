ed69e2bd20cb06deddd2067666787b28d08478f3
mythpoy <okmlnsunok@gmail.com>
2023-03-31 18:07:46 +0900

    feat : service di 형식 변경 및 좋아요 개수 높은순으로 부스 불러오는 API 개발.

--- src/main/java/com/hallym/festival/domain/likes/repository/LikeRepository.java
+++ src/main/java/com/hallym/festival/domain/likes/repository/LikeRepository.java
@@ -1,7 +1,11 @@
 package com.hallym.festival.domain.likes.repository;
 
+import com.hallym.festival.domain.booth.entity.Booth;
 import com.hallym.festival.domain.likes.entity.Likes;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
--- src/main/java/com/hallym/festival/domain/likes/service/LikeService.java
+++ src/main/java/com/hallym/festival/domain/likes/service/LikeService.java
@@ -1,12 +1,16 @@
 package com.hallym.festival.domain.likes.service;
 
+import com.hallym.festival.domain.booth.dto.PageRequestDTO;
+import com.hallym.festival.domain.booth.dto.PageResponseDTO;
 import com.hallym.festival.domain.likes.dto.LikesResponseDto;
+import com.hallym.festival.domain.likes.dto.LikesResponseTopDto;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface LikeService {
     String likeTrigger(Long bno, HttpServletRequest request, HttpServletResponse response);
+    PageResponseDTO<LikesResponseTopDto> getTopLikeBoothList(PageRequestDTO pageRequestDTO);
     int getCount(Long boothId);
 
 }

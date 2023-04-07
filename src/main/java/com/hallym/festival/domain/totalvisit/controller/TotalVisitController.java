fa1ac66dfc155ad366340c7c9821b792caa18c27
mythpoy <okmlnsunok@gmail.com>
2023-04-07 13:03:44 +0900

    feat : TotalVisitController 생성

---
+++ src/main/java/com/hallym/festival/domain/totalvisit/controller/TotalVisitController.java
@@ -0,0 +1,29 @@
+package com.hallym.festival.domain.totalvisit.controller;
+
+import com.hallym.festival.domain.totalvisit.dto.TotalVisitCountDTO;
+import com.hallym.festival.domain.totalvisit.entity.TotalVisit;
+import com.hallym.festival.domain.totalvisit.service.TotalVisitService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping()
+@RequiredArgsConstructor
+public class TotalVisitController {
+
+    private final TotalVisitService totalVisitService;
+
+    @GetMapping()
+    public void visit(TotalVisit dto) {
+        totalVisitService.visit(dto);
+    }
+
+    @GetMapping("/visit-list")
+    public List<TotalVisitCountDTO> visitList() {
+        return totalVisitService.visitList();
+    }
+}

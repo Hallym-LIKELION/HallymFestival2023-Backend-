9f623d3b07865086740b7e6d063fd400b1a3dfb2
mythpoy <okmlnsunok@gmail.com>
2023-04-07 13:03:07 +0900

    feat : totalVisit Entity 생성 (tno, visitDate)

---
+++ src/main/java/com/hallym/festival/domain/totalvisit/entity/TotalVisit.java
@@ -0,0 +1,32 @@
+package com.hallym.festival.domain.totalvisit.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "totalvisit")
+@Builder
+@EntityListeners(value = { AuditingEntityListener.class }) //Audting 기능 추가
+@Entity
+public class TotalVisit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "totalvisit_id")
+    private Long tno;
+
+    @CreatedDate //생성시간 자동저장
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Column(name = "visitDate", updatable = false)
+    private LocalDate visitDate;
+}

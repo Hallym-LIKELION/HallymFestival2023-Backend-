406f17d3cce3f01a14b126fc50b9254bc900cffb
mythpoy <okmlnsunok@gmail.com>
2023-05-04 13:02:52 +0900

    refactor : entity 별로 color 추가

--- src/main/java/com/hallym/festival/domain/visitcomment/dto/VisitCommentResponseDTO.java
+++ src/main/java/com/hallym/festival/domain/visitcomment/dto/VisitCommentResponseDTO.java
@@ -1,5 +1,6 @@
 package com.hallym.festival.domain.visitcomment.dto;
 
+import com.hallym.festival.domain.visitcomment.entity.Color;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,4 +14,5 @@ public class VisitCommentResponseDTO {
     private Long vno;
     private String content;
     private String ip;
+    private Color color;
 }

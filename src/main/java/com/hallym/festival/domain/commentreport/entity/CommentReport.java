15e39c03316d4ef0d440b39631a3b41b89f68ce9
mythpoy <okmlnsunok@gmail.com>
2023-05-10 12:34:07 +0900

    feat : commentReport 중복 ip 쿠키생성 방지

--- src/main/java/com/hallym/festival/domain/commentreport/entity/CommentReport.java
+++ src/main/java/com/hallym/festival/domain/commentreport/entity/CommentReport.java
@@ -2,6 +2,7 @@ package com.hallym.festival.domain.commentreport.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.hallym.festival.domain.comment.entity.Comment;
+import com.hallym.festival.global.baseEntity.BaseTimeRegDateEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,13 +15,15 @@ import javax.persistence.*;
 @AllArgsConstructor
 @Builder
 @Entity
-public class CommentReport {
+public class CommentReport extends BaseTimeRegDateEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long crno;
 
     @NotNull
     private String cookieKey;
+
+    private String ip;
 
     //json backreferecne 2개 넣으면 오류난대서 value 지정하기.
     @JsonBackReference(value="comment") //부모클래스에 JsonBackReference 붙여서 순환참조 방어.
